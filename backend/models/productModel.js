8bd4548ee9e13db5adf6dbf734d926174e8f9a90
LOGESHWARAN <https://github.com/LOGESHWARAN7588/OTP-App>
2023-04-22 18:44:09 +0530

    backend creating product model, date seeder

---
+++ backend/models/productModel.js
@@ -0,0 +1,93 @@
+const  mongoose = require('mongoose');
+
+const productschema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, "please enter product name"],
+        trim : true,
+        maxLength: [100, "product name cannot exceed 100 characters"]
+    },
+    price:{
+        type: Number,
+        required : true,
+        default: 0.0
+
+    },
+    description:{
+        type : String,
+        required:[true,"please enter product description"]
+    },
+    ratings:{
+        type: String,
+        default:0
+    },
+    images:[
+        {
+            image:{
+                type: String,
+                required: true
+            }
+
+        }
+    ],
+    category:{
+        type: String,
+        required:[true, "Please enter product category"],
+        enum:{
+            values:[
+                'Electronics',
+                'Mobile Phones',
+                'Laptops',
+                'Accessories',
+                'Headphones',
+                'Food',
+                'Books',
+                'Clothes/shoes',
+                'Beauty/Health',
+                'sports',
+                'Outdoor',
+                'Home'
+            ],
+            message:"Please enter correct category"
+        }
+    },
+    seller:{
+        type: String,
+        required:[true, "Please enter product seller"]
+    },
+    stock:{
+        type: Number,
+        required:[true, "Please enter product stock"],
+        maxLength: [20,'product stock cannot exceed 20']
+    },
+    numOfReviews:{
+        type: Number,
+        default:0
+    },
+    reviews:[
+        {
+            name:{
+                type: String,
+                required: true
+            },
+            ratings:{
+                type:String,
+                required:true
+            },
+            Comment:{
+                type: String,
+                required:true
+            }
+        }
+    ],
+    createdAt:{
+        type: Date,
+        default:Date.now()
+    }
+    
+
+})
+
+let  schema =  mongoose.model('Product', productschema )
+
+module.exports =schema
