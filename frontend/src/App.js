7a490d185ef759c2c0ed1e9f48ebbd6976a039c6
LOGESHWARAN <https://github.com/LOGESHWARAN7588/OTP-App>
2023-04-29 20:51:44 +0530

    Creating React Project, header & Footer, create Home component, React router setup, custom title using React-Helmey-Async

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import React  from 'react';
+import Header from './components/layouts/Header';
+import Footer from './components/layouts/Footer';
+import Home from './components/Home';
+import {  Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import {HelmetProvider} from 'react-helmet-async'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <HelmetProvider> 
+        <Header/>
+          <Routes>
+            <Route path ='/' element={<Home/>}/>
+          </Routes>
+        <Footer/>
+      </HelmetProvider>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/layouts/MetaData.js
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet-async"
+import React from 'react';
+
+export default function MetaData({title}){
+    return(
+        <Helmet>
+            <title>
+                {`${title   }- kumarancart`}
+            </title>
+        </Helmet>
+
+    )
+
+}
