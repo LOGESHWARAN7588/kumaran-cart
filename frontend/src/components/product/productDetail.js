1dab965db7f7cabbb9a7cf7a0b2d90de9e69d682
LOGESHWARAN <https://github.com/LOGESHWARAN7588/OTP-App>
2023-05-14 13:45:58 +0530

    Add to Cart Button action

--- frontend/src/components/product/productDetail.js
+++ frontend/src/components/product/productDetail.js
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 import Loader from '../layouts/Loader'
 import {Carousel} from 'react-bootstrap';
 import MetaData from "../layouts/MetaData";
+import { addCartItem } from "../../actions/cartActions";
 
 export default function ProductDetail(){
    const {loading, product} = useSelector((state) => state.productState);
@@ -72,7 +73,10 @@ export default function ProductDetail(){
 
                 <span className="btn btn-primary plus" onClick={increaseQty}>+</span>
             </div>
-             <button type="button" id="cart_btn" disabled={product.stock===0?true:false} className="btn btn-primary d-inline ml-4">Add to Cart</button>
+             <button type="button" id="cart_btn" 
+             disabled={product.stock===0?true:false} 
+             onClick={()=>dispatch(addCartItem(product._id, quantity))}
+             className="btn btn-primary d-inline ml-4">Add to Cart</button>
 
             <hr />
 
