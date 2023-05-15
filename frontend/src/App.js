ae5a24f7fb744b5982e71dab1e27f68c88d581dc
LOGESHWARAN <https://github.com/LOGESHWARAN7588/OTP-App>
2023-05-15 21:06:58 +0530

    Frontend Stripe Integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Home from './components/Home';
 import Footer from './components/layouts/Footer';
 import Header from './components/layouts/Header';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { ToastContainer} from 'react-toastify';
@@ -22,13 +22,22 @@ import ResetPassword from './components/user/ResetPassword';
 import Cart from './components/cart/Cart';
 import Shipping from './components/cart/Shipping';
 import ConfirmOrder from './components/cart/ConfirmOrder';
+import Payment from './components/cart/Payment';
+import axios from 'axios';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
 
 function App() {
+  const [stripeApiKey, setStripeApiKey] = useState("")
 
 
   useEffect(()=>{
     store.dispatch(loadUser)
+    async function getStripeApiKey(){
+      const {data} = await axios.get('/api/v1/stripeapi')
+      setStripeApiKey(data.stripeApiKey)
+     }
 
   })
 
@@ -57,6 +66,7 @@ function App() {
               <Route path='/cart' element={<Cart/>}/>
               <Route path='/shipping' element={<ProtectedRoute><Shipping/></ProtectedRoute>}/>
               <Route path='/order/confirm' element={<ProtectedRoute><ConfirmOrder/></ProtectedRoute>}/>
+              <Route path='/payment' element={<ProtectedRoute><Elements stripe={loadStripe(stripeApiKey)}><Payment/></Elements></ProtectedRoute>}/>
 
 
 
