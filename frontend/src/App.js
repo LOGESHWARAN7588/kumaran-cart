8f022b1439680700c364a39a76f3e3d01322864d
LOGESHWARAN <https://github.com/LOGESHWARAN7588/OTP-App>
2023-06-06 14:46:00 +0530

    Dashboard Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -86,7 +86,7 @@ function App() {
             </Routes>
             </div>
             <Routes>
-            <Route path ='/admin/dashboard' element={<Dashboard/>}/>
+            <Route  path ='/admin/dashboard' element={<ProtectedRoute isAdmin={true}><Dashboard/></ProtectedRoute>}/>
             </Routes>
             <Footer/>
        </HelmetProvider>
