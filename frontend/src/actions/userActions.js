3f01962985e457be5d222bb19617f0ad924e2704
LOGESHWARAN <https://github.com/LOGESHWARAN7588/OTP-App>
2023-06-11 09:55:49 +0530

    Admin: users/ List/ Update/ Delete

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -27,6 +27,22 @@ import {
 
 } from "../slices/authSlice"
 
+import {
+    usersRequest,
+    usersSuccess,
+    usersFail,
+    userRequest,
+    userSuccess,
+    userFail,
+    deleteUserRequest,
+    deleteUserSuccess,
+    deleteUserFail,
+    updateUserRequest,
+    updateUserSuccess,
+    updateUserFail
+
+} from '../slices/userSlice'
+
 export const login =(email , password) => async (dispatch)=>{
 
     try{
@@ -163,4 +179,57 @@ export const resetPassword = (formData, token) => async (dispatch)=>{
         dispatch(resetPasswordFail(error.response.data.message))
 
     }
+}
+
+export const getUsers =  async (dispatch) => {
+
+    try {
+        dispatch(usersRequest())
+        const { data }  = await axios.get(`/api/v1/admin/users`);
+        dispatch(usersSuccess(data))
+    } catch (error) {
+        dispatch(usersFail(error.response.data.message))
+    }
+
+}
+
+export const getUser = id => async (dispatch) => {
+
+    try {
+        dispatch(userRequest())
+        const { data }  = await axios.get(`/api/v1/admin/user/${id}`);
+        dispatch(userSuccess(data))
+    } catch (error) {
+        dispatch(userFail(error.response.data.message))
+    }
+
+}
+
+export const deleteUser = id => async (dispatch) => {
+
+    try {
+        dispatch(deleteUserRequest())
+        await axios.delete(`/api/v1/admin/user/${id}`);
+        dispatch(deleteUserSuccess())
+    } catch (error) {
+        dispatch(deleteUserFail(error.response.data.message))
+    }
+
+}
+
+export const updateUser = (id, formData) => async (dispatch) => {
+
+    try {
+        dispatch(updateUserRequest())
+        const config = {
+            headers: {
+                'Content-type': 'application/json'
+            }
+        }
+        await axios.put(`/api/v1/admin/user/${id}`, formData, config);
+        dispatch(updateUserSuccess())
+    } catch (error) {
+        dispatch(updateUserFail(error.response.data.message))
+    }
+
 }
